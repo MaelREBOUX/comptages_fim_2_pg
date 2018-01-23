3f0d1039c2c23406ce8b2aeb0a45479e214c8910
MaelReboux <m.reboux@rennesmetropole.fr>
2018-01-23 13:40:45 +0100

    renommage des identifiants internes

--- comptage_routier.sql
+++ comptage_routier.sql
@@ -3,10 +3,10 @@
 -- SIG Rennes Métropole
 
 
--- DROP TABLE mobilite_transp.comptage_enquete ;
+-- DROP TABLE mobilite_transp.comptage_enquete CASCADE ;
 CREATE TABLE mobilite_transp.comptage_enquete
 (
-  enquete_id serial NOT NULL,
+  enquete_uid serial NOT NULL,
   comm_insee varchar(5) NOT NULL,
   description text NOT NULL,
   site text,
@@ -19,7 +19,7 @@ CREATE TABLE mobilite_transp.comptage_enquete
   d_routier varchar(3),
   d_vitesse varchar(3),
   -- pk
-  CONSTRAINT mobilite_transp_comptage_enquete_pkey PRIMARY KEY (enquete_id)
+  CONSTRAINT mobilite_transp_comptage_enquete_pkey PRIMARY KEY (enquete_uid)
 )
 WITH (
   OIDS=FALSE
@@ -29,10 +29,11 @@ WITH (
 
 
 
--- DROP TABLE mobilite_transp.comptage_station ;
+-- DROP TABLE mobilite_transp.comptage_station CASCADE ;
 CREATE TABLE mobilite_transp.comptage_station
 (
-  station_id serial NOT NULL,
+  station_uid serial NOT NULL,
+  
   comm_insee varchar(5) NOT NULL,
   materiel text,
   type integer,
@@ -50,7 +51,7 @@ CREATE TABLE mobilite_transp.comptage_station
   CONSTRAINT enforce_geotype_shape CHECK (geometrytype(shape) = 'POINT'::text),
   CONSTRAINT enforce_srid_shape CHECK (st_srid(shape) = 3948),
   -- pk
-  CONSTRAINT mobilite_transp_comptage_station_pkey PRIMARY KEY (station_id)
+  CONSTRAINT mobilite_transp_comptage_station_pkey PRIMARY KEY (station_uid)
 )
 WITH (
   OIDS=FALSE
@@ -63,8 +64,8 @@ WITH (
 -- DROP TABLE mobilite_transp.comptage_automatique ;
 CREATE TABLE mobilite_transp.comptage_automatique
 (
-  enquete_id integer, -- NOT NULL,
-  station_id integer NOT NULL,
+  enquete_uid integer, -- NOT NULL,
+  station_uid integer NOT NULL,
   date_tmst timestamp without time zone, -- 2016-10-11 00:00:00
   date_str varchar(10),  -- 11/10/2016
   heure_deb integer,
@@ -78,7 +79,7 @@ CREATE TABLE mobilite_transp.comptage_automatique
   nb_2r integer,
   nb_pieton integer,
   -- pk
-  CONSTRAINT mobilite_transp_comptage_automatique_pkey PRIMARY KEY (enquete_id, station_id, date_tmst, heure_deb)
+  CONSTRAINT mobilite_transp_comptage_automatique_pkey PRIMARY KEY (enquete_uid, station_uid, date_tmst, heure_deb)
 )
 WITH (
   OIDS=FALSE
@@ -136,7 +137,7 @@ INSERT INTO mobilite_transp.comptage_dom_station_type VALUES (5,'piéton hors pa
 CREATE VIEW mobilite_transp.v_comptage_station_automatique AS
   SELECT
     row_number() over() as uid,
-    b.station_id,
+    b.station_uid,
     b.comm_insee,
     b.type,
     b.sens,
@@ -155,7 +156,7 @@ CREATE VIEW mobilite_transp.v_comptage_station_automatique AS
     a.nb_pieton,
     b.shape::geometry(Point,3948) AS shape
   FROM mobilite_transp.comptage_automatique a
-    LEFT JOIN mobilite_transp.comptage_station b ON a.station_id = b.station_id ;
+    LEFT JOIN mobilite_transp.comptage_station b ON a.station_uid = b.station_uid ;
 
 
 -- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -163,8 +164,8 @@ CREATE VIEW mobilite_transp.v_comptage_station_automatique AS
 
 -- les droits pour le compte geocarto en écriture
 GRANT USAGE ON SCHEMA mobilite_transp TO geocarto ;
-GRANT ALL ON SEQUENCE mobilite_transp.comptage_enquete_enquete_id_seq TO geocarto;
-GRANT ALL ON SEQUENCE mobilite_transp.comptage_station_station_id_seq TO geocarto;
+GRANT ALL ON SEQUENCE mobilite_transp.comptage_enquete_enquete_uid_seq TO geocarto;
+GRANT ALL ON SEQUENCE mobilite_transp.comptage_station_station_uid_seq TO geocarto;
 GRANT ALL ON TABLE mobilite_transp.comptage_automatique TO geocarto;
 GRANT ALL ON TABLE mobilite_transp.comptage_dom_station_sens TO geocarto;
 GRANT ALL ON TABLE mobilite_transp.comptage_dom_station_type TO geocarto;
@@ -174,8 +175,8 @@ GRANT ALL ON TABLE mobilite_transp.v_comptage_station_automatique TO geocarto;
 
 -- les droits en lecture pour consult
 GRANT USAGE ON SCHEMA mobilite_transp TO consult ;
-GRANT SELECT ON SEQUENCE mobilite_transp.comptage_enquete_enquete_id_seq TO consult;
-GRANT SELECT ON SEQUENCE mobilite_transp.comptage_station_station_id_seq TO consult;
+GRANT SELECT ON SEQUENCE mobilite_transp.comptage_enquete_enquete_uid_seq TO consult;
+GRANT SELECT ON SEQUENCE mobilite_transp.comptage_station_station_uid_seq TO consult;
 GRANT SELECT ON TABLE mobilite_transp.comptage_automatique TO consult;
 GRANT SELECT ON TABLE mobilite_transp.comptage_dom_station_sens TO consult;
 GRANT SELECT ON TABLE mobilite_transp.comptage_dom_station_type TO consult;
